72d8442b54ca9ee4d8fc9835f6cf72514503808a
ekim3483 <ekim3483@gmail.com>
2025-11-10 17:04:16 -0800

    Delete Backup_restore_automation directory

--- Backup_restore_automation/truncate_tables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
- 
-DATABASE=sales
-
-mysql -Nse 'show tables' sales | \
-    while read table; do mysql \
-    -e "use sales;SET FOREIGN_KEY_CHECKS=0;truncate table $table;SET FOREIGN_KEY_CHECKS=1;" ;done
